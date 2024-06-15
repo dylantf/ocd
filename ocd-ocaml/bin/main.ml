61407079f187b60c2619b70d733bcbb716a7eda0
dylantf <dylantf@gmail.com>
2024-06-15 16:15:48 +0200

    Formatting

--- ocd-ocaml/bin/main.ml
+++ ocd-ocaml/bin/main.ml
@@ -1,14 +1,12 @@
-let parse_csv_file =
-  List.map (fun row ->
-      match row with
-      | [ a; b ] -> (Int64.of_string a, b)
-      | _ -> failwith "CSV row formatted incorrectly")
+let parse_csv_file rows =
+  List.tl rows
+  |> List.map (fun row ->
+         match row with
+         | [ a; b ] -> (Int64.of_string a, b)
+         | _ -> failwith "CSV row formatted incorrectly")
 
 let () =
-  let filename = "file.csv" in
-  let csv_data =
-    Csv.load ~separator:';' filename |> List.tl |> parse_csv_file
-  in
+  let csv_data = Csv.load "file.csv" ~separator:';' |> parse_csv_file in
   List.iter
     (fun (oc_id, inserted_at) -> Printf.printf "%Ld %s\n" oc_id inserted_at)
     csv_data
